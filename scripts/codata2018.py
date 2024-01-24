5ee05f3d514befb47ea5e05c0e3838ac65d8841b
viniciusdutra314 <dutravinisousa2@usp.br>
2024-01-24 11:55:31 -0300

    submodulo constantes adicionado
    todas as constantes do CODATA2018 agora são acessiveis, falta realizar testes e documentar

--- scripts/codata2018.py
+++ scripts/codata2018.py
@@ -1,13 +1,19 @@
+'''Script usado na criação do arquivo constantes/constantes.py
+Ele essencialmente baixa as constantes definidas pela CODATA2018
+e transforma o arquivo .txt em variáveis python acessíveis
+pelo submódulo
+'''
+
 import os
 from subprocess import run
-from .medida import Medida
-
-abs_path=os.path.dirname(__file__)
 file_name='codata2018_constantes.txt'
+abs_path=os.path.dirname(__file__)
 file_dir=os.path.join(abs_path,file_name)
+
 if file_name not in os.listdir(abs_path):
     run(["curl","https://physics.nist.gov/cuu/Constants/Table/allascii.txt",
           "-o" ,file_dir])
+
 with open(file_dir,'r') as txt:
     #esses números mágicos fazem muito sentido se
     #você abrir o arquivo de texto
@@ -15,9 +21,14 @@ with open(file_dir,'r') as txt:
     constantes_dict={}
     for line in lines:
         name=line[:60].rstrip().replace(" ","_")
-        nominal=float(line[60:85].rstrip().replace(" ","").replace(".",""))
+        name=name.replace('-','_').replace('.','').replace("/","_over_")
+        name=name.replace('(','').replace(')','').replace(',','')
+        nominal=float(line[60:85].rstrip().replace(" ","").replace("...",""))
         incerteza=line[85:110].rstrip().replace(" ","")
         if incerteza=="(exact)": incerteza=0
         else: incerteza=float(incerteza)
         unidade=line[110:].strip()
         constantes_dict[name]=[nominal,incerteza,unidade]
+with open(os.path.join(abs_path,'constantes.py'),'w') as python_arquivo:
+    for name, values in constantes_dict.items():
+        python_arquivo.write(f'{name}={values}\n')
